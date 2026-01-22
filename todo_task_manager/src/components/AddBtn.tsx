540af1df80912a48d12d82f3977eb83e6bebb9f0
Htun Thiha (Steve) <htunthiha.dev01@gmail.com>
2026-01-22 20:23:31 +0800

    feat(todo_task_manager: add_btn_scroll): Add Add task btn, fix scroll inconsistency

---
+++ todo_task_manager/src/components/AddBtn.tsx
@@ -0,0 +1,11 @@
+function AddBtn() {
+  return(
+    <>
+      <button className="btn" id="add-btn">
+        <i className="bi bi-patch-plus"></i>&ensp;Add Task
+      </button>
+    </>
+  );
+}
+
+export default AddBtn;
